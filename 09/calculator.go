70fa46b0c7cc9479d7d51a077fd39ead9ce6a3dc
vanjara <vanjara@outlook.com>
2020-07-26 15:58:50 -0700

    adding my exercise solutions

---
+++ 09/calculator.go
@@ -0,0 +1,13 @@
+package calculator
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func Divide(a int, b int) int {
+	return a / b
+}
